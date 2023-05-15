42aa743796c3d19a908aec82ad9613569df6e62c
unknown <krsmanovic013@gmail.com>
2023-05-16 00:02:09 +0200

    added modal and modal logic

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import CartContainer from "./components/CartContainer";
 import { calculateTotals } from "./features/cart/cartSlice";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import Modal from "./components/Modal";
 
 const App = () => {
   const dispatch = useDispatch();
   const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
 
   useEffect(() => {
     dispatch(calculateTotals());
@@ -14,6 +16,7 @@ const App = () => {
 
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 import CartItem from "./CartItem";
-import { clearCart } from "../features/cart/cartSlice";
+import { openModal } from "../features/modal/modalSlice";
 
 const CartContainer = () => {
   const dispatch = useDispatch();
@@ -33,7 +33,7 @@ const CartContainer = () => {
             total <span>${total.toFixed(0)}</span>
           </h4>
         </div>
-        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+        <button className="btn clear-btn" onClick={() => dispatch(openModal())}>
           clear cart
         </button>
       </footer>
